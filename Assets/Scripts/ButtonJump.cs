0dd19e1dc6c436077f65627cae3031d84ba62a67
KucingMaboek <45931684+KucingMaboek@users.noreply.github.com>
2020-10-05 13:36:13 +0700

    update asset and packages

---
+++ Assets/Scripts/ButtonJump.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonJump : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public CharacterController characterController;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        characterController.IsJumping = true;
+    }
+
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        characterController.IsJumping = false;
+    }
+}
---
+++ Assets/Scripts/ButtonMoveRight.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonMoveRight : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public CharacterController characterController;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        characterController.IsMovingRight = true;
+    }
+
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        characterController.IsMovingRight = false;
+    }
+}
