6168ae6fa14f3736073f01fc1cc1e91273d5d1e2
KucingMaboek <45931684+KucingMaboek@users.noreply.github.com>
2020-10-23 03:35:50 +0700

    add basic stage system

---
+++ Assets/Scripts/MenuLevelSelection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+public class MenuLevelSelection : UIController
+{
+    private int chapter;
+    public RectTransform stageContainer;
+    public GameObject backgroundPanel;
+
+    public void SelectChapter(int chapter)
+    {
+        this.chapter = chapter;
+        backgroundPanel.SetActive(true);
+        StartCoroutine(AnimationWideOut(stageContainer, 0.15f));
+    }
+
+    public void UnselectChapter()
+    {
+        chapter = 0;
+        backgroundPanel.SetActive(false);
+        StartCoroutine(AnimationWideIn(stageContainer, 0.15f));
+    }
+
+    public void SelectStage(int stage)
+    {
+        String loadLevel = "Chapter_" + chapter + "Stage" + stage;
+        SceneLoader(loadLevel);
+    }
+}
